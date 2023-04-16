86b78823b3d7877758af27e3d733a5119479a6f9
MitalBhanushali <64398899+MittalMav@users.noreply.github.com>
2023-04-16 01:25:08 -0400

    commit

--- build.sbt
+++ build.sbt
@@ -11,11 +11,15 @@ scalacOptions ++= Seq(
 	"-Ywarn-value-discard",
 	"-Ywarn-unused"
 )
+val sparkVersion = "3.3.1"
 
 // Dependencies
 libraryDependencies ++= Seq(
+	"org.scalatest" %% "scalatest" % "3.2.14" % "test",
+	"org.apache.spark" %% "spark-sql" % sparkVersion,
+	"org.apache.spark" %% "spark-core" % sparkVersion,
+	"org.apache.spark" %% "spark-mllib" % sparkVersion
 )
-
 // Execution & Test Options
 mainClass in compile := Some("edu.neu.coe.csye7200.Entry")
 
--- src/main/scala/edu/neu/coe/csye7200/Entry.scala
+++ src/main/scala/edu/neu/coe/csye7200/Entry.scala
@@ -1,5 +1,58 @@
 package edu.neu.coe.csye7200
 
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions.{col, date_format, to_date, to_timestamp, unix_timestamp}
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types.StringType
+
 object Entry extends App {
 	println("This works!")
+
+	val spark = SparkSession.builder()
+		.appName("Crime Rate")
+		.master("local[*]")
+		.getOrCreate()
+	spark.sparkContext.setLogLevel("ERROR")
+
+	val crimeLADF = spark.read.format("csv")
+		.option("header", "true")
+		.option("inferSchema", "true")
+		.load("D:/Scala2/CSYE7200_FinalProject/src/test/resources/Crime_Data_LA_2020_to_Present.csv")
+
+	//val colToKeep = Array("DATE OCC","TIME OCC","Crm Cd Desc","Premis Desc","LAT","LON")
+	val dfCrimeLA = crimeLADF.select(to_date(substring(col("DATE OCC"), 1, 10), "MM/dd/yyyy").as("to_date"),
+		col("TIME OCC"),
+		col("Crm Cd Desc"),
+		col("Premis Desc"),
+		col("LAT"),
+		col("LON"))
+
+	dfCrimeLA.select(concat(col("to_date"), lit(" "), lpad(col("TIME OCC").cast(StringType), 4, "0"))).show(10)
+	val dfCrimeLA1 = dfCrimeLA.select(to_timestamp(concat(col("to_date"), lit(" "), lpad(col("TIME OCC").cast(StringType), 4, "0")), "yyyy-MM-dd HHmm").alias("Crime_Time"),
+		col("Crm Cd Desc").as("Crime_Type"),
+		col("Premis Desc").as("Crime_Loc"),
+		col("LAT").as("Latitude"),
+		col("LON").as("Longitude")
+	)
+	println("**********************LA from 2020 to present******************************")
+	dfCrimeLA1.show(20)
+	dfCrimeLA1.printSchema()
+
+	val crimeCHDF = spark.read.format("csv")
+		.option("header", "true")
+		.option("inferSchema", "true")
+		.load("D:/Scala2/CSYE7200_FinalProject/src/test/resources/Crimes_CH_2001_to_Present.csv")
+
+
+	val dfCrimeCH = crimeCHDF.select(to_timestamp(substring(col("Date"), 1, 19), "MM/dd/yyyy HH:mm:ss").alias("Crime_Time"),
+		col("Description").as("Crime_Type"),
+		col("Location Description").as("Crime_Loc"),
+		col("Latitude").as("Latitude"),
+		col("Longitude").as("Longitude")
+	)
+	println("**********************************Chicago from 2001 to present********************************************")
+	dfCrimeCH.show(20)
+	dfCrimeCH.printSchema()
+
+
 }
